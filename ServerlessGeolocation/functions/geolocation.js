af36291d92398bdbe1003a34403d1f162591cbd8
Ivan <gibson.ivan@gmail.com>
2020-10-27 11:56:44 -0400

    added some netlify stuff

---
+++ ServerlessGeolocation/functions/geolocation.js
@@ -0,0 +1,4 @@
+exports.handler = async (event) => {
+    const subject = event.queryStringParameters.personLocation
+    return `hi`
+}
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let latDisplay = document.querySelector(".lat");
 let lngDisplay = document.querySelector(".lng");
+let personLocation = [0, 0];
+// latDisplay.innerHTML = "Your Latitude " + lat;
+// lngDisplay.innerHTML = "Your Longitude" + lng;
 navigator.geolocation.watchPosition(success, error, options);
 var map = L.map('mapid').setView([51.505, -0.09], 13);
 var options = {
@@ -17,21 +20,24 @@ L.marker([51.505, -0.09]).addTo(map)
     .openPopup();
     
 function success(pos) {
-    // var {latitude, longitude} = pos.coords;
-    // const coords = {latitude, longitude};
-    // console.log(coords);
-    // map.setView(coords, 13);
-    
     var crd = pos.coords;
     const lat = crd.latitude;
     const lng = crd.longitude;
-    console.log(lat);
-    latDisplay.innerHTML = "Your Latitude " + lat;
-    lngDisplay.innerHTML = "Your Longitude" + lng;
+    personLocation.push(lat, lng)
+    // console.log(lat);
     L.marker([lat, lng]).addTo(map)
     .bindPopup(`Latitude: ${lat} <br> Longitude: ${lng}`)
     .openPopup();
     map.setView([lat, lng], 13);
+    locationLog(personLocation);
+}
+
+async function locationLog(personLocation){
+    const responce = await fetch(`https://elated-payne-827778.netlify.app/.netlify/functions/hello?vote=${personLocation}`);
+    console.log(responce);
+    const data = await responce.json();
+    console.log(data);
+    // map.setView(personLocation, 13);
 }
     
 function error(err) {
